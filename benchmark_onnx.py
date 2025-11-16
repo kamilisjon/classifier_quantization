8c31f88da9e28f75260f12a7dad72136abfffa01
KamilisJonkus <kamilis.jonkus@agmis.com>
2025-11-16 20:56:55 +0200

    refactor

--- benchmark_onnx.py
+++ benchmark_onnx.py
@@ -1,18 +1,48 @@
 import argparse
+import time
+from pathlib import Path
+from enum import Enum
 import csv
 import json
-from pathlib import Path
 
-import numpy as np
 import onnxruntime
+import numpy as np
 
 from pre_process import load_and_preprocess
 
+WARMUP_RUNS_COUNT = 25
+BENCHMARK_RUNS_COUNT = 50
 IMAGENET_LABELS_FILEPATH = "imagenet_class_index.json"
 
+class ExecProvider(Enum):
+    CPU = 0
+    CUDA = 1
+    TRT_FP32 = 2
+    TRT_FP16 = 3
+    TRT_INT8 = 4
 
-def benchmark_accuracy(model_path: Path, imagenet_data_path: Path):
+    def __repr__(self):
+        return self.name
 
+BENCHMARK_EXEC_PROVIDERS = [ExecProvider.CUDA, ExecProvider.TRT_FP32, ExecProvider.TRT_FP16, ExecProvider.TRT_INT8]
+
+def setup_session(model_path: Path, exec_provider: ExecProvider) -> onnxruntime.InferenceSession:
+    session_options = onnxruntime.SessionOptions()
+    session_options.log_severity_level = 0
+    session_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_DISABLE_ALL
+
+    match exec_provider:
+        case ExecProvider.CPU: provider = ['CPUExecutionProvider']
+        case ExecProvider.CUDA: provider = ['CUDAExecutionProvider']
+        case ExecProvider.TRT_FP32: provider = ['TensorrtExecutionProvider']
+        case ExecProvider.TRT_FP16: provider = [('TensorrtExecutionProvider', {'trt_fp16_enable': True})]
+        case ExecProvider.TRT_INT8: provider = [('TensorrtExecutionProvider', 
+                                                 {'trt_fp16_enable': True, 'trt_int8_enable': True,
+                                                  'trt_int8_calibration_table_name': str(model_path.parent / "calibration.flatbuffers")})]
+    return onnxruntime.InferenceSession(str(model_path), sess_options=session_options, providers=provider)
+
+
+def benchmark_accuracy(session: onnxruntime.InferenceSession, imagenet_data_path: Path):
     val_dir = imagenet_data_path / 'ILSVRC' / 'Data' / 'CLS-LOC' / "val"
     images = sorted([f for f in val_dir.iterdir() if f.suffix.upper() == '.JPEG'])
 
@@ -51,24 +81,11 @@ def benchmark_accuracy(model_path: Path, imagenet_data_path: Path):
         ground_truth.append(gt_indices)
     print(f"Built ground_truth for {len(ground_truth)} images")
 
-    # ONNX session
-    session_options = onnxruntime.SessionOptions()
-    # 0 = VERBOSE, 1 = INFO, 2 = WARNING, 3 = ERROR, 4 = FATAL
-    session_options.log_severity_level = 0  # INFO
-    session_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_DISABLE_ALL
-    provider = [('TensorrtExecutionProvider', {
-        'trt_fp16_enable': False,
-        'trt_int8_enable': False,
-        'trt_int8_calibration_table_name': str(model_path.parent / "calibration.flatbuffers")
-    })]
-    provider = ['CUDAExecutionProvider']
-    ort_session = onnxruntime.InferenceSession(str(model_path), sess_options=session_options, providers=provider)
-
     # Benchmark
     correct_top1 = correct_top5 = total = 0
     for i, img_path in enumerate(images):
         input_tensor = load_and_preprocess(img_path)
-        outputs = ort_session.run(None, {"x": input_tensor})[0]
+        outputs = session.run(None, {"x": input_tensor})[0]
         pred = np.argsort(outputs[0])[-5:][::-1]
 
         gt_indices = set(ground_truth[i])
@@ -92,11 +109,40 @@ def benchmark_accuracy(model_path: Path, imagenet_data_path: Path):
 
     top1_acc = correct_top1 / total * 100
     top5_acc = correct_top5 / total * 100
-    print(f"\nTop-1: {top1_acc:.2f}% | Top-5: {top5_acc:.2f}%")
+    return top1_acc, top5_acc
+
+def benchmark_speed(session: onnxruntime.InferenceSession, batch_size: int):
+    input_data = np.zeros((batch_size, 3, 224, 224), np.float32)
+    input_ortvalue = onnxruntime.OrtValue.ortvalue_from_numpy(input_data, 'cuda', 0)
+
+    iobinding = session.io_binding()
+    iobinding.bind_ortvalue_input(session.get_inputs()[0].name, input_ortvalue)
+    for output in session.get_outputs():
+        iobinding.bind_output(output.name, 'cuda')
+
+    # Warm up
+    for _ in range(WARMUP_RUNS_COUNT):
+        session.run_with_iobinding(iobinding)
+
+    total_duration = 0.0
+    for _ in range(BENCHMARK_RUNS_COUNT):
+        start = time.perf_counter()
+        session.run_with_iobinding(iobinding)
+        batch_duration = (time.perf_counter() - start) * 1000
+        total_duration += batch_duration
+        print(f"{batch_duration:.2f}ms")
+    return total_duration / BENCHMARK_RUNS_COUNT
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("model_path", type=Path)
-    parser.add_argument("data_dir", type=Path)
+    parser.add_argument("data_path", type=Path)
+    parser.add_argument("batch_size", type=int)
     args = parser.parse_args()
-    benchmark_accuracy(args.model_path, args.data_dir)
+    results = {}
+    for exec_provider in BENCHMARK_EXEC_PROVIDERS:
+        session = setup_session(args.model_path, exec_provider)
+        speed = benchmark_speed(session, args.batch_size)
+        top1_acc, top5_acc = benchmark_accuracy(session, args.data_path)
+        results[exec_provider] = {"top1_acc": top1_acc, "top5_acc": top5_acc, "speed": round(speed, 3)}
+    print(results)
